f948ae6c2ecbcf50d91540a7e84ce4958412610c
LydianLights <rane.fields@gmail.com>
2016-11-28 04:13:04 -0800

    added video list and enforced min window size

--- YouTube Video Download/MainWindow.xaml.cs
+++ YouTube Video Download/MainWindow.xaml.cs
@@ -28,9 +28,46 @@ namespace YouTubeVideoDownload
         }
 
 
-        private void buttonGetVideo_Click(object sender, RoutedEventArgs e)
+
+        private void buttonStartDownload_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void buttonPauseDownload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonStopDownload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void buttonAddVideos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonClearVideoList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        // Resize the video list headers when the list is resized
+        // Columns[0] = Video Title
+        // Columns[1] = Progress
+        private void listViewVideoList_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double progressBarWidth = 100;
+            double titleColumnWidth = listViewVideoList.ActualWidth - progressBarWidth;
+
+            GridView view = (GridView)listViewVideoList.View;
+            view.Columns[1].Width = progressBarWidth;
+            view.Columns[0].Width = (titleColumnWidth > 0) ? titleColumnWidth : 0;
         }
     }
 }
